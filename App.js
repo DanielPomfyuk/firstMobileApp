033058564e9ce3ea8b2b9a0ea89acc8a18aca370
DanielPomfyuk <danyapomfyuk99@gmail.com>
2020-05-30 14:52:11 +0100

    Swearing app

--- App.js
+++ App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View ,Button} from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View ,Button,TextInput} from 'react-native';
 
 export default function App() {
+  const [enteredWord,setEnteredWord] = useState('');
+  const [currentPhrase,setCurrentPhrase] = useState('');
+  const pressHandler = (text)=>{
+    setEnteredWord(text)
+  }
+  const addHandler = ()=>{
+    setCurrentPhrase(currentPhrase=>[...currentPhrase," ",enteredWord])
+    setEnteredWord('')
+  }
   return (
     <View style={styles.container}>
-      <Button title="Press to puk"></Button>
-      <Text style={styles.text}>Zalupa</Text>
+      <View style={styles.enterArea}>
+      <TextInput 
+      placeholder="Ebalo" 
+      onChangeText={pressHandler} 
+      value={enteredWord}
+      style={styles.input}>  
+      </TextInput >
+      <Button title="Add" onPress={addHandler}></Button>
+      </View>
+      <View style={styles.textHolder}>
+  <Text style={styles.text}> You are a {currentPhrase}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'black',
-    color:"white",
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    backgroundColor: "#ff6361",
+  },
+  enterArea:{
+    marginTop:50,
+    marginLeft:30,
+    width:"80%%",
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems:"center"
+  },
+  input:{
+    width:"90%",
+    height:40,
+    backgroundColor: 'white',
+    borderWidth:1,
+    color:"black",
+    fontSize:20
   },
   text:{
+    textAlign:"center",
     color:"white",
     fontSize:30
+  },
+  textHolder:{
+    margin:30,
+    width:"80%"
   }
 });
